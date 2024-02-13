9e84178fabecdf394b5bb489bb82609c16a5a9a5
DJayNyne <157189086+DJayNyne@users.noreply.github.com>
2024-02-12 21:40:07 -0700

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,63 +1,30 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
-import './ExpenseItem.css'
 
-const ExpenseItem = (props) => {
-const { dispatch } = useContext(AppContext);
-
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
-    };
-
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
-
-
-    }
-
-    const decreaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
-        dispatch({
-            type: 'RED_EXPENSE',
-            payload: expense
-        });
-    }
+const ExpenseList = ({ selectedCurrency }) => {
+    const { expenses } = useContext(AppContext);
 
     return (
-        <tr>
-        <td>{props.name}</td>
-        <td>{props.cost}</td>
-        <td><button
-        className="btn-increase" 
-        onClick={event=> increaseAllocation(props.name)}>
-            +
-            </button>
-            </td>
-        <td><button
-        className="btn-decrease"
-        onClick={event=> decreaseAllocation(props.name)}>
-            -
-            </button>
-            </td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
+        <table className='table'>
+              <thead className="thead-light">
+            <tr>
+              <th scope="col">Department</th>
+              <th scope="col">Allocated Budget</th>
+              <th scope="col">Increase by 10</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+            <tbody>
+            {expenses.map((expense) => (
+                <ExpenseItem
+                key={expense.id}
+                name={expense.name}
+                cost={`${selectedCurrency}${expense.cost}`} />
+            ))}
+            </tbody>
+        </table>
     );
 };
 
-export default ExpenseItem;
+export default ExpenseList;
