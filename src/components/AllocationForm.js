d2daa48c4c3b7e02cd48283c57cad34ce695f7f5
DJayNyne <157189086+DJayNyne@users.noreply.github.com>
2024-02-12 21:37:42 -0700

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import './AllocationForm.css';
 
-const AllocationForm = (props) => {
+const AllocationForm = ({ selectedCurrency }) => {
     const { dispatch,remaining  } = useContext(AppContext);
 
     const [name, setName] = useState('');
@@ -51,20 +52,23 @@ const AllocationForm = (props) => {
                 <option value="Admin" name="admin">Admin</option>
                   </select>
 
-                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                    <div className="input-group-prepend" style={{ marginLeft: '0rem' }}>
                 <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
                   </div>
                   <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
+                  <div className="input-group-prepend">
+                    <span className="currency-symbol">{selectedCurrency}</span>
+                  </div>
 
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '0rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
